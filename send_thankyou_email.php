8400515b86c88383b3ee8da48b35b21fecb4d9c4
ailiefraser <cafraser@cs.ucsd.edu>
2014-10-30 13:11:50 -0700

    send thank you email and error if user id missing

---
+++ send_thankyou_email.php
@@ -0,0 +1,38 @@
+<?php
+function send_email($email_address) {
+
+	// Send email to user
+	require 'phpmailer/PHPMailerAutoload.php';
+
+	$mail = new PHPMailer;
+	//$mail->SMTPDebug = 3;
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'GradStudioProject@gmail.com';
+	$mail->Password = 'feedbackisgreat';
+	$mail->SMTPSecure = 'tls';
+	$mail->Port = 587;
+	$mail->From = 'GradStudioProject@gmail.com';
+	$mail->FromName = "Grad Studio";
+	$mail->addAddress($email_address);
+	$mail->WordWrap = 50;
+	$mail->isHTML(true);
+
+	$mail->Subject = "Thank you for participating in the Grad Studio Project!";
+	
+	
+	$text = "<p>Thanks for being in the GradStudio Project! </p>
+			<p>Good luck with school and stuff</p>";
+	
+	$mail->Body = $text;
+	$mail->AltBody = strip_tags($text);
+
+	if (!$mail->send()) {
+		echo 'Problem sending email: ' . $mail->ErrorInfo;
+	} else if ($show_confirm) {
+		echo "Email successfully sent!";
+	}
+}
+
+?>
---
+++ thankyou.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML>
+<!--
+	MegaCorp by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+
+<html>
+<head>
+<title>GradStudio</title>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800" rel="stylesheet" type="text/css" />
+<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/skel.min.js"></script>
+<!--<script src="js/skel-panels.min.js"></script>-->
+<script src="js/init.js"></script>
+<noscript>
+	<link rel="stylesheet" href="css/skel-noscript.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/style-desktop.css" />
+</noscript>
+
+<style type="text/css">
+html {
+	height: 100%;
+}
+</style>
+
+</head>
+<body>
+<div id="body_wrapper">
+<!-- Header -->
+    <div id="header-wrapper-small">
+
+	<div id="header" class="container">
+		<div id="logo"><h1><a href="#">GradStudio</a></h1></div>
+                <nav id="nav">
+                    <ul>
+                        <li><a href="index.html">Homepage</a></li>
+                        <li><a href="instructions.html">Instructions</a></li>
+			<li><a href="faq.html">FAQ</a></li>
+			<li><a href="aboutus.html">About Us</a></li>
+                    </ul>
+                </nav>
+		
+	</div>
+</div>
+
+<div id="page">
+<div class="container">
+<div class="row">
+
+<div id="content" class="12u skel-cell-important">
+<article>
+<header>
+	<h2 class="main-title">Thank you!</h2>
+</header>
+
+<p>Thanks for completing the end survey! You have now completed the Grad Studio Project!
+You should receive a confirmation email from us shortly.</p>
+<p style="color: red;">
+<?php
+include 'send_thankyou_email.php';
+session_start();
+$email_address = $_SESSION['email_address'];
+send_thankyou_email($email_address);
+
+session_destroy();
+?>
+</p>
+
+</article>
+</div>
+</div>
+</div>
+</div>
+<div id="footer_push"></div>
+</div>
+<!-- Copyright -->
+<div id="copyright" class="container">
+	Design: <a href="http://templated.co">TEMPLATED</a>
+	Images: © Catherine M. Hicks
+</div>
+
+</body>
+</html>
